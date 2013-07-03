623099245fc20e0e55d11d5015b10e0284b4cd46
Zhou Shuo <pretzel.zhou@gmail.com>
2013-07-03 17:11:31 +0800

    add work_item

--- api/project/urls.py
+++ api/project/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import patterns, url
 
 from .views import projects_view, project_view, project_status_list_view, project_status_detail_view, \
-                   releases_view, release_view, release_status_list_view, release_status_detail_view
+                   releases_view, release_view, release_status_list_view, release_status_detail_view, \
+                   work_item_groups_view, work_item_group_view, work_items_by_group_view, work_items_by_release_view, \
+                   work_item_view
+                   
 
 
 urlpatterns = patterns('',
@@ -14,4 +17,10 @@ urlpatterns = patterns('',
     url(r'^(?i)release/(?P<release_id>([0-9])+)/$', release_view.as_view(), name="release_view"),
     url(r'^(?i)release/(?P<release_id>([0-9])+)/release-status$', release_status_list_view.as_view(), name="release_status_list_view"),
     url(r'^(?i)release-status/(?P<release_status_id>([0-9])+)/$', release_status_detail_view.as_view(), name="release_status_detail_view"),
+    url(r'^(?i)project/(?P<project_id>([0-9])+)/work-item-groups/$', work_item_groups_view.as_view(), name="project_work_item_groups_view"),
+    url(r'^(?i)work-item-group/(?P<work_item_group_id>([0-9])+)/$', work_item_group_view.as_view(), name="work_item_group_view"),
+    url(r'^(?i)work-item-group/(?P<work_item_group_id>([0-9])+)/work-items$', work_items_by_group_view.as_view(), name="work_items_by_group_view"),
+    url(r'^(?i)release/(?P<release_id>([0-9])+)/work-items$', work_items_by_release_view.as_view(), name="work_items_by_release_view"),
+    url(r'^(?i)work-item/(?P<work_item_id>([0-9])+)/$', work_item_view.as_view(), name="work_item_view"),
+    
 )
--- api/project/views.py
+++ api/project/views.py
@@ -3,9 +3,9 @@ from rest_framework import mixins
 
 
 from api.secure.models import user
-from .models import project, project_status, release, release_status
+from .models import project, project_status, release, release_status, work_item_group, work_item
 from .serializers import project_serializer, project_status_serializer, release_serializer, \
-                         release_status_serializer
+                         release_status_serializer, work_item_group_serializer, work_item_serializer
 
 
 class projects_view(generics.GenericAPIView,
@@ -270,5 +270,165 @@ class release_status_detail_view(generics.GenericAPIView,
          
         return self.destroy(request)
     
+class work_item_groups_view(generics.GenericAPIView,
+                            mixins.CreateModelMixin,
+                            mixins.ListModelMixin):
+    queryset = work_item_group.objects.all()
+    serializer_class = work_item_group_serializer
     
+    def get(self, request, project_id=0):
+        if project_id > 0:
+            self.kwargs['project.pk'] = project_id
+            
+        return self.list(request)
+    
+    def post(self, request, project_id=0):
+        if project_id > 0:
+            self.kwargs['project.pk'] = project_id
+        
+        return self.create(request)
+    
+    def get_queryset(self):
+        if 'project.pk' in self.kwargs:
+            try:
+                input_project = project.objects.get(pk=self.kwargs['project.pk'])
+                return work_item_group.objects.filter(project=input_project)
+            except project.DoesNotExist:
+                return work_item_group.objects.filter(project=None)
+        
+        return work_item_group.objects.all()
+    
+class work_item_group_view(generics.GenericAPIView,
+                           mixins.CreateModelMixin,
+                           mixins.UpdateModelMixin,
+                           mixins.RetrieveModelMixin,
+                           mixins.DestroyModelMixin):
+    queryset = work_item_group.objects.all()
+    serializer_class = work_item_group_serializer
+    
+    def get(self, request, work_item_group_id=0):    
+        if 'pk' not in self.kwargs:
+            self.kwargs['pk'] = work_item_group_id  
+             
+        return self.retrieve(request)
+       
+    def post(self, request, work_item_group_id=0):  
+        if 'pk' not in self.kwargs:
+            self.kwargs['pk'] = work_item_group_id  
+               
+        if work_item_group_id > 0:
+            try:
+                work_item_group.objects.get(pk=work_item_group_id)
+                return self.update(request)
+            except release.DoesNotExist:
+                return self.create(request)
+               
+        return self.create(request)
+
+    def put(self, request, work_item_group_id=0):  
+        if 'pk' not in self.kwargs:
+            self.kwargs['pk'] = work_item_group_id  
+               
+        return self.update(request)
+
+    def delete(self, request, work_item_group_id=0):  
+        if 'pk' not in self.kwargs:
+            self.kwargs['pk'] = work_item_group_id  
+               
+        return self.destroy(request)
+        
+class work_items_by_group_view(generics.GenericAPIView,
+                              mixins.CreateModelMixin,
+                              mixins.ListModelMixin):
+    queryset = work_item.objects.all()
+    serializer_class = work_item_serializer
+    
+    def get(self, request, work_item_group_id=0):
+        if work_item_group_id > 0:
+            self.kwargs['work_item_group.pk'] = work_item_group_id
+            
+        return self.list(request)
+    
+    def post(self, request, work_item_group_id=0):
+        if work_item_group_id > 0:
+            self.kwargs['work_item_group.pk'] = work_item_group_id
+        
+        return self.create(request)
+    
+    def get_queryset(self):
+        if 'work_item_group.pk' in self.kwargs:
+            try:
+                input_work_item_group = work_item_group.objects.get(pk=self.kwargs['work_item_group.pk'])
+                return work_item.objects.filter(work_item_group=input_work_item_group)
+            except project.DoesNotExist:
+                return work_item.objects.filter(work_item_group=None)
+        
+        return work_item.objects.all()
+      
+class work_items_by_release_view(generics.GenericAPIView,
+                                 mixins.CreateModelMixin,
+                                 mixins.ListModelMixin):
+    queryset = work_item.objects.all()
+    serializer_class = work_item_serializer
+    
+    def get(self, request, release_id=0):
+        if release_id > 0:
+            self.kwargs['release.pk'] = release_id
+            
+        return self.list(request)
+    
+    def post(self, request, release_id=0):
+        if release_id > 0:
+            self.kwargs['release.pk'] = release_id
+        
+        return self.create(request)
+    
+    def get_queryset(self):
+        if 'release.pk' in self.kwargs:
+            try:
+                input_release = release.objects.get(pk=self.kwargs['release.pk'])
+                return work_item.objects.filter(release=input_release)
+            except project.DoesNotExist:
+                return work_item.objects.filter(release=None)
+        
+        return work_item.objects.all()
+       
+class work_item_view(generics.GenericAPIView,
+                     mixins.CreateModelMixin,
+                     mixins.UpdateModelMixin,
+                     mixins.RetrieveModelMixin,
+                     mixins.DestroyModelMixin):
+    queryset = work_item.objects.all()
+    serializer_class = work_item_serializer
+    
+    def get(self, request, work_item_id=0):    
+        if 'pk' not in self.kwargs:
+            self.kwargs['pk'] = work_item_id  
+             
+        return self.retrieve(request)
+       
+    def post(self, request, work_item_id=0):  
+        if 'pk' not in self.kwargs:
+            self.kwargs['pk'] = work_item_id  
+               
+        if work_item_id > 0:
+            try:
+                work_item.objects.get(pk=work_item_id)
+                return self.update(request)
+            except release.DoesNotExist:
+                return self.create(request)
+               
+        return self.create(request)
+
+    def put(self, request, work_item_id=0):  
+        if 'pk' not in self.kwargs:
+            self.kwargs['pk'] = work_item_id  
+               
+        return self.update(request)
+
+    def delete(self, request, work_item_id=0):  
+        if 'pk' not in self.kwargs:
+            self.kwargs['pk'] = work_item_id  
+               
+        return self.destroy(request)
     
