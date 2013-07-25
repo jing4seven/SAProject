26060b3eaea330a1ed6d3ebc65e0c1a2616be65b
Jing <jing@ubuntu.ubuntu-domain>
2013-07-25 10:41:47 +0800

    add  logic to project view'

--- api/project/views.py
+++ api/project/views.py
@@ -1,28 +1,34 @@
+import hashlib
 from rest_framework import generics
 from rest_framework import mixins
-
-
+from lib.wrapper import condition
 from api.secure.models import user
 from .models import project, project_status, release, release_status, work_item_group, work_item
 from .serializers import project_serializer, project_status_serializer, release_serializer, \
                          release_status_serializer, work_item_group_serializer, work_item_serializer
-
 
 class projects_view(generics.GenericAPIView,
                     mixins.ListModelMixin,
                     mixins.CreateModelMixin):
     queryset = project.objects.all()
     serializer_class = project_serializer
-    
+
+    def get_projects_etag(request, *args, **kwargs):
+        ct = project.objects.count()
+        latest_row = project.objects.raw('SELECT id, update_time FROM sa_t_projects ORDER BY update_time DESC LIMIT 1')
+        return hashlib.md5("%s$%s" % (ct, latest_row[0].update_time)).hexdigest()
+
+    def get_projects_last_modified(reuqest, *args, **kwargs):
+        latest_row = project.objects.raw('SELECT id, update_time FROM sa_t_projects ORDER BY update_time DESC LIMIT 1')
+        return latest_row[0].update_time
+
+    @condition(etag_func=get_projects_etag, last_modified_func=get_projects_last_modified)
     def get(self, request, owner_username=None):
         if owner_username is not None:
             self.kwargs['owner.username'] = owner_username
-            
+
         return self.list(request)
-    
-    def post(self, request, owner_id=0):        
-        return self.create(request, owner_id=owner_id)
-    
+
     def get_queryset(self):
         if 'owner.username' in self.kwargs:
             try:
@@ -30,60 +36,61 @@ class projects_view(generics.GenericAPIView,
                 return project.objects.filter(owner=owner)
             except user.DoesNotExist:
                 return project.objects.get_empty_query_set()
-                
+
         return project.objects.all()
+
 
 class project_view(generics.GenericAPIView,
                    mixins.CreateModelMixin,
                    mixins.UpdateModelMixin,
                    mixins.RetrieveModelMixin,
                    mixins.DestroyModelMixin):
-    
+
     queryset = project.objects.all()
     serializer_class = project_serializer
-    
-    def get(self, request, project_id=0):      
+
+    def get(self, request, project_id=0):
         if 'pk' not in self.kwargs:
-            self.kwargs['pk'] = project_id  
-            
+            self.kwargs['pk'] = project_id
+
         return self.retrieve(request)
-       
-    def post(self, request, project_id=0):  
+
+    def post(self, request, project_id=0):
         if 'pk' not in self.kwargs:
-            self.kwargs['pk'] = project_id 
-                  
+            self.kwargs['pk'] = project_id
+
         return self.create(request)
 
     def put(self, request, project_id=0):
         if 'pk' not in self.kwargs:
-            self.kwargs['pk'] = project_id 
-        
+            self.kwargs['pk'] = project_id
+
         return self.update(request)
 
     def delete(self, request, project_id=0):
         if 'pk' not in self.kwargs:
-            self.kwargs['pk'] = project_id 
-            
+            self.kwargs['pk'] = project_id
+
         return self.destroy(request)
-    
+
 class project_status_list_view(generics.GenericAPIView,
                                mixins.ListModelMixin,
                                mixins.CreateModelMixin):
     queryset = project_status.objects.all()
-    serializer_class = project_status_serializer    
-    
+    serializer_class = project_status_serializer
+
     def get(self, request, project_id=0):
         if project_id > 0:
             self.kwargs['project.pk'] = project_id
-            
+
         return self.list(request)
 
-    def post(self, request, project_id=0): 
+    def post(self, request, project_id=0):
         if project_id > 0:
             self.kwargs['project.pk'] = project_id
-                   
+
         return self.create(request)
-    
+
     def get_queryset(self):
         if 'project.pk' in self.kwargs:
             try:
@@ -91,67 +98,67 @@ class project_status_list_view(generics.GenericAPIView,
                 return project_status.objects.filter(project=input_project)
             except project.DoesNotExist:
                 return project_status.objects.filter(project=None)
-                
+
         return project_status.objects.all()
-    
+
 class project_status_detail_view(generics.GenericAPIView,
                                  mixins.CreateModelMixin,
                                  mixins.UpdateModelMixin,
                                  mixins.RetrieveModelMixin,
                                  mixins.DestroyModelMixin):
-    
+
     queryset = project_status.objects.all()
     serializer_class = project_status_serializer
-    
-    def get(self, request, project_status_id=0):    
+
+    def get(self, request, project_status_id=0):
         if 'pk' not in self.kwargs:
-            self.kwargs['pk'] = project_status_id  
-            
+            self.kwargs['pk'] = project_status_id
+
         return self.retrieve(request)
-    
+
     def post(self, request, project_status_id=0):
         if 'pk' not in self.kwargs:
-            self.kwargs['pk'] = project_status_id  
-            
+            self.kwargs['pk'] = project_status_id
+
         if project_status_id > 0:
             try:
                 project_status.objects.get(pk=project_status_id)
                 return self.update(request)
             except release.DoesNotExist:
                 return self.create(request)
-                    
+
         return self.create(request)
 
     def put(self, request, project_status_id=0):
         if 'pk' not in self.kwargs:
-            self.kwargs['pk'] = project_status_id  
-         
+            self.kwargs['pk'] = project_status_id
+
         return self.update(request)
 
     def delete(self, request, project_status_id=0):
         if 'pk' not in self.kwargs:
-            self.kwargs['pk'] = project_status_id  
-         
+            self.kwargs['pk'] = project_status_id
+
         return self.destroy(request)
-    
+
 class releases_view(generics.GenericAPIView,
                      mixins.ListModelMixin,
                      mixins.CreateModelMixin):
     queryset = release.objects.all()
     serializer_class = release_serializer
-    
+
     def get(self, request, project_id=0):
         if project_id > 0:
             self.kwargs['project.pk'] = project_id
-            
+
         return self.list(request)
 
-    def post(self, request, project_id=0):   
+    def post(self, request, project_id=0):
         if project_id > 0:
             self.kwargs['project.pk'] = project_id
-             
+
         return self.create(request)
-    
+
     def get_queryset(self):
         if 'project.pk' in self.kwargs:
             try:
@@ -159,47 +166,47 @@ class releases_view(generics.GenericAPIView,
                 return release.objects.filter(project=input_project)
             except project.DoesNotExist:
                 return release.objects.get_empty_query_set()
-                
+
         return release.objects.all()
-    
+
 class release_view(generics.GenericAPIView,
                    mixins.CreateModelMixin,
                    mixins.UpdateModelMixin,
                    mixins.RetrieveModelMixin,
                    mixins.DestroyModelMixin):
-    
+
     queryset = release.objects.all()
     serializer_class = release_serializer
-    
-    def get(self, request, release_id=0):    
+
+    def get(self, request, release_id=0):
         if 'pk' not in self.kwargs:
-            self.kwargs['pk'] = release_id  
-             
+            self.kwargs['pk'] = release_id
+
         return self.retrieve(request)
-       
-    def post(self, request, release_id=0):  
+
+    def post(self, request, release_id=0):
         if 'pk' not in self.kwargs:
-            self.kwargs['pk'] = release_id  
-               
+            self.kwargs['pk'] = release_id
+
         if release_id > 0:
             try:
                 release.objects.get(pk=release_id)
                 return self.update(request)
             except release.DoesNotExist:
                 return self.create(request)
-               
+
         return self.create(request)
 
-    def put(self, request, release_id=0):  
+    def put(self, request, release_id=0):
         if 'pk' not in self.kwargs:
-            self.kwargs['pk'] = release_id  
-               
+            self.kwargs['pk'] = release_id
+
         return self.update(request)
 
-    def delete(self, request, release_id=0):  
+    def delete(self, request, release_id=0):
         if 'pk' not in self.kwargs:
-            self.kwargs['pk'] = release_id  
-               
+            self.kwargs['pk'] = release_id
+
         return self.destroy(request)
 
 class release_status_list_view(generics.GenericAPIView,
@@ -207,19 +214,19 @@ class release_status_list_view(generics.GenericAPIView,
                                mixins.CreateModelMixin):
     queryset = release_status.objects.all()
     serializer_class = release_status_serializer
-    
+
     def get(self, request, release_id):
         if release_id > 0:
             self.kwargs['release.pk'] = release_id
-            
+
         return self.list(request)
-    
+
     def post(self, request, release_id):
         if release_id > 0:
             self.kwargs['release.pk'] = release_id
-            
+
         return self.create(request)
-    
+
     def get_queryset(self):
         if 'release.pk' in self.kwargs:
             try:
@@ -227,67 +234,67 @@ class release_status_list_view(generics.GenericAPIView,
                 return release_status.objects.filter(release=input_release)
             except release.DoesNotExist:
                 return release_status.objects.filter(release=None)
-            
+
         return release_status.objects.all()
-    
+
 class release_status_detail_view(generics.GenericAPIView,
                                  mixins.CreateModelMixin,
                                  mixins.UpdateModelMixin,
                                  mixins.RetrieveModelMixin,
                                  mixins.DestroyModelMixin):
-    
+
     queryset = release_status.objects.all()
     serializer_class = release_status_serializer
-    
-    def get(self, request, release_status_id=0):    
+
+    def get(self, request, release_status_id=0):
         if 'pk' not in self.kwargs:
-            self.kwargs['pk'] = release_status_id  
-            
+            self.kwargs['pk'] = release_status_id
+
         return self.retrieve(request)
-    
+
     def post(self, request, release_status_id=0):
         if 'pk' not in self.kwargs:
-            self.kwargs['pk'] = release_status_id  
-            
+            self.kwargs['pk'] = release_status_id
+
         if release_status_id > 0:
             try:
                 release_status.objects.get(pk=release_status_id)
                 return self.update(request)
             except release.DoesNotExist:
                 return self.create(request)
-                    
+
         return self.create(request)
 
     def put(self, request, release_status_id=0):
         if 'pk' not in self.kwargs:
-            self.kwargs['pk'] = release_status_id  
-         
+            self.kwargs['pk'] = release_status_id
+
         return self.update(request)
 
     def delete(self, request, release_status_id=0):
         if 'pk' not in self.kwargs:
-            self.kwargs['pk'] = release_status_id  
-         
+            self.kwargs['pk'] = release_status_id
+
         return self.destroy(request)
-    
+
 class work_item_groups_view(generics.GenericAPIView,
                             mixins.CreateModelMixin,
                             mixins.ListModelMixin):
     queryset = work_item_group.objects.all()
     serializer_class = work_item_group_serializer
-    
+
     def get(self, request, project_id=0):
         if project_id > 0:
             self.kwargs['project.pk'] = project_id
-            
+
         return self.list(request)
-    
+
     def post(self, request, project_id=0):
         if project_id > 0:
             self.kwargs['project.pk'] = project_id
-        
+
         return self.create(request)
-    
+
     def get_queryset(self):
         if 'project.pk' in self.kwargs:
             try:
@@ -295,9 +302,9 @@ class work_item_groups_view(generics.GenericAPIView,
                 return work_item_group.objects.filter(project=input_project)
             except project.DoesNotExist:
                 return work_item_group.objects.filter(project=None)
-        
+
         return work_item_group.objects.all()
-    
+
 class work_item_group_view(generics.GenericAPIView,
                            mixins.CreateModelMixin,
                            mixins.UpdateModelMixin,
@@ -305,56 +312,56 @@ class work_item_group_view(generics.GenericAPIView,
                            mixins.DestroyModelMixin):
     queryset = work_item_group.objects.all()
     serializer_class = work_item_group_serializer
-    
-    def get(self, request, work_item_group_id=0):    
+
+    def get(self, request, work_item_group_id=0):
         if 'pk' not in self.kwargs:
-            self.kwargs['pk'] = work_item_group_id  
-             
+            self.kwargs['pk'] = work_item_group_id
+
         return self.retrieve(request)
-       
-    def post(self, request, work_item_group_id=0):  
+
+    def post(self, request, work_item_group_id=0):
         if 'pk' not in self.kwargs:
-            self.kwargs['pk'] = work_item_group_id  
-               
+            self.kwargs['pk'] = work_item_group_id
+
         if work_item_group_id > 0:
             try:
                 work_item_group.objects.get(pk=work_item_group_id)
                 return self.update(request)
             except release.DoesNotExist:
                 return self.create(request)
-               
+
         return self.create(request)
 
-    def put(self, request, work_item_group_id=0):  
+    def put(self, request, work_item_group_id=0):
         if 'pk' not in self.kwargs:
-            self.kwargs['pk'] = work_item_group_id  
-               
+            self.kwargs['pk'] = work_item_group_id
+
         return self.update(request)
 
-    def delete(self, request, work_item_group_id=0):  
+    def delete(self, request, work_item_group_id=0):
         if 'pk' not in self.kwargs:
-            self.kwargs['pk'] = work_item_group_id  
-               
+            self.kwargs['pk'] = work_item_group_id
+
         return self.destroy(request)
-        
+
 class work_items_by_group_view(generics.GenericAPIView,
                               mixins.CreateModelMixin,
                               mixins.ListModelMixin):
     queryset = work_item.objects.all()
     serializer_class = work_item_serializer
-    
+
     def get(self, request, work_item_group_id=0):
         if work_item_group_id > 0:
             self.kwargs['work_item_group.pk'] = work_item_group_id
-            
+
         return self.list(request)
-    
+
     def post(self, request, work_item_group_id=0):
         if work_item_group_id > 0:
             self.kwargs['work_item_group.pk'] = work_item_group_id
-        
+
         return self.create(request)
-    
+
     def get_queryset(self):
         if 'work_item_group.pk' in self.kwargs:
             try:
@@ -362,27 +369,27 @@ class work_items_by_group_view(generics.GenericAPIView,
                 return work_item.objects.filter(work_item_group=input_work_item_group)
             except project.DoesNotExist:
                 return work_item.objects.filter(work_item_group=None)
-        
+
         return work_item.objects.all()
-      
+
 class work_items_by_release_view(generics.GenericAPIView,
                                  mixins.CreateModelMixin,
                                  mixins.ListModelMixin):
     queryset = work_item.objects.all()
     serializer_class = work_item_serializer
-    
+
     def get(self, request, release_id=0):
         if release_id > 0:
             self.kwargs['release.pk'] = release_id
-            
+
         return self.list(request)
-    
+
     def post(self, request, release_id=0):
         if release_id > 0:
             self.kwargs['release.pk'] = release_id
-        
+
         return self.create(request)
-    
+
     def get_queryset(self):
         if 'release.pk' in self.kwargs:
             try:
@@ -390,9 +397,9 @@ class work_items_by_release_view(generics.GenericAPIView,
                 return work_item.objects.filter(release=input_release)
             except project.DoesNotExist:
                 return work_item.objects.filter(release=None)
-        
+
         return work_item.objects.all()
-       
+
 class work_item_view(generics.GenericAPIView,
                      mixins.CreateModelMixin,
                      mixins.UpdateModelMixin,
@@ -400,35 +407,35 @@ class work_item_view(generics.GenericAPIView,
                      mixins.DestroyModelMixin):
     queryset = work_item.objects.all()
     serializer_class = work_item_serializer
-    
-    def get(self, request, work_item_id=0):    
+
+    def get(self, request, work_item_id=0):
         if 'pk' not in self.kwargs:
-            self.kwargs['pk'] = work_item_id  
-             
+            self.kwargs['pk'] = work_item_id
+
         return self.retrieve(request)
-       
-    def post(self, request, work_item_id=0):  
+
+    def post(self, request, work_item_id=0):
         if 'pk' not in self.kwargs:
-            self.kwargs['pk'] = work_item_id  
-               
+            self.kwargs['pk'] = work_item_id
+
         if work_item_id > 0:
             try:
                 work_item.objects.get(pk=work_item_id)
                 return self.update(request)
             except release.DoesNotExist:
                 return self.create(request)
-               
+
         return self.create(request)
 
-    def put(self, request, work_item_id=0):  
+    def put(self, request, work_item_id=0):
         if 'pk' not in self.kwargs:
-            self.kwargs['pk'] = work_item_id  
-               
+            self.kwargs['pk'] = work_item_id
+
         return self.update(request)
 
-    def delete(self, request, work_item_id=0):  
+    def delete(self, request, work_item_id=0):
         if 'pk' not in self.kwargs:
-            self.kwargs['pk'] = work_item_id  
-               
+            self.kwargs['pk'] = work_item_id
+
         return self.destroy(request)
-    
+
